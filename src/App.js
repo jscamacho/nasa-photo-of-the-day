2e8ac5b312ff2ab993dda50d947807a113b26c81
Jason Camacho <jasoncamacho@Jasons-MBP.attlocal.net>
2019-08-01 20:14:23 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import {useEffect,useState} from "react";
 import "./App.css";
+import axios from 'axios';
+
+import About from "./components/about";
+import Date from "./components/date";
+import Footer from "./components/footer";
+import Media from "./components/media";
 
 function App() {
+
+const [file, setFile] = useState();
+
+useEffect(()=>{
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then((info)=>{
+      setFile(info.data);
+      console.log(info.data);
+    })
+    .catch(error=>{
+      console.log("major error");
+    })
+}, []);
+
   return (
     <div className="App">
       <p>
